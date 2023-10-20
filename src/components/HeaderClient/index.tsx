0f6821c6c35c0fba97f0e6509b4315df3a03381c
DennerOl <dener155@hotmail.com>
2023-10-19 22:07:18 -0300

    Componente HeaderClient

---
+++ src/components/HeaderClient/index.tsx
@@ -0,0 +1,23 @@
+import cartIcon from '../../assets//imagens/imagemCarrinho.svg';
+export default function HeaderClient() {
+
+  return (
+    <header className="dsc-header-client">
+      <nav className="dsc-container">
+        <h1>DsCommerce</h1>
+
+        <div className="dsc-navbar-right">
+
+          <div className="dsc-menu-items-container">
+            <div className="dsc-menu-item">
+              <img src={cartIcon} alt="carrinhoCompras" />
+            </div>
+          </div>
+          <a href="#"> Entrar</a>
+        </div>
+      </nav>
+    </header>
+
+  );
+
+}
