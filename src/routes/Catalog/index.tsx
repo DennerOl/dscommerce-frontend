35e6b5deb12515ce39379c9fda3ec334d329460c
DennerOl <dener155@hotmail.com>
2023-10-29 18:46:00 -0300

    Componentes da pagina de Catalog

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,6 +1,8 @@
 import HeaderClient from '../../components/HeaderClient'
 import './styles.css'
-import computerImg from '../../assets/imagens/computer.png';
+import SearchBar from '../../components/SearchBar';
+import CatalogCard from '../../components/CatalogCard';
+import ButtonNextPage from '../../components/ButtonNextPage';
 
 export default function Catalog() {
 
@@ -9,148 +11,21 @@ export default function Catalog() {
       <HeaderClient />
       <main>
         <section id="catalog-section" className="dsc-container">
-          <form className="dsc-search-bar">
-            <button>🔎︎ </button>
-            <input type="text" placeholder="Nome do produto" />
-            <button type="reset"> 🗙</button>
-
-          </form>
-
+          <SearchBar />
           <div className=" dsc-catalog-cards  dsc-mb20  dsc-mt20">
-
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top  dsc-line-bottom">
-                <img src={computerImg} alt="computador" />
-              </div>
-
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT com suporte e 16GB de memória e processador
-                  turbo plus
-                </h4>
-              </div>
-            </div>
-
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
           </div>
-
-          <div className="dsc-btn-next-page">
-            carregar mais
-          </div>
-
+          <ButtonNextPage />
         </section>
 
       </main>
