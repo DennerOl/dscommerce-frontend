7fe5a1419c292fa43c820481f79fbe83ebf4ce57
DennerOl <dener155@hotmail.com>
2023-11-16 20:38:00 -0300

    Rota dinamica buscando produto por id

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <Route index element={<Catalog />} />
 
           <Route path="catalog" element={<Catalog />} />
-          <Route path="product-details" element={<ProductDetails />} />
+          <Route path="product-details/:productId" element={<ProductDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,40 +2,25 @@ import './styles.css';
 import ButtonInverse from '../../../components/ButtonInverse';
 import ButtonPrimary from '../../../components/ButtonPrimary';
 import ProductDetailsCard from '../../../components/ProductDetailsCard';
-import { ProductDTO } from '../../../models/product';
+import * as productservice from '../../../Services/product-service';
+import { useParams } from 'react-router-dom';
 
-const product: ProductDTO = {
-  id: 2,
-  name: "Smart TV",
-  description: "TV boa ",
-  imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
-  price: 2500.99,
-  categories: [
-    {
-      id: 2,
-      name: "Eletrônicos"
-    },
-    {
-      id: 3,
-      name: "Computadores"
-    },
 
-    {
-      id: 4,
-      name: "Importados"
-    }
 
-  ]
-}
 
 export default function ProductDetails() {
+
+  /* uso o params para redenrizar um produto pelo id dele*/
+  const params = useParams();
+  const product = productservice.findById(Number(params.productId));
+
+
   return (
-
-
     <main>
       <section id="product-details-section" className="dsc-container">
-        <ProductDetailsCard product={product} />
-
+        {product &&
+          <ProductDetailsCard product={product} />
+        }
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
           <ButtonInverse text="Início" />
