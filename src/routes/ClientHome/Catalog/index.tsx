595af353987755c74152b77449268f9b08de6941
DennerOl <dener155@hotmail.com>
2023-11-28 19:42:34 -0300

    Exemplo de localStorage comentado Catalog

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,6 +5,7 @@ import ButtonNextPage from '../../../components/ButtonNextPage';
 import * as productservice from '../../../Services/product-service'
 import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
+import { CategoryDTO } from '../../../models/category';
 
 
 
@@ -15,7 +16,21 @@ export default function Catalog() {
 
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
+  /*
+    const objTest: CategoryDTO = {
+      id: 8,
+      name: "Jardinagem"
+    }
+  */
+
+
   useEffect(() => {
+    /* ----salva o objeto como string no localStorage
+        localStorage.setItem("minhaCategoria", JSON.stringify(objTest));
+    
+    --- aqui pego um string salvo no localStorage e converto para obj
+        const obj = JSON.parse(localStorage.getItem("minhaCategoria")  || "{}" );
+    */
     productservice.findAll()
       .then(resposta => {
         setProducts(resposta.data.content);
