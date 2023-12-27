5cce86427a6ec1eda55b1ad0326c3521a00ed215
DennerOl <dener155@hotmail.com>
2023-12-26 21:36:19 -0300

    criando o requestBackend para URL

---
+++ src/utils/requests.ts
@@ -0,0 +1,7 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { Base_URL } from './system';
+
+
+export function requestBackend(config: AxiosRequestConfig) {
+  return axios({ ...config, baseURL: Base_URL });
+}
