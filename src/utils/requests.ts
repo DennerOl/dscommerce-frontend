f0b4019bc072352960a868713d0680dcb1223d78
DennerOl <dener155@hotmail.com>
2024-01-02 22:13:11 -0300

    Melhorando o metodo requestBackend

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,7 +1,24 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { Base_URL } from './system';
+import * as authService from '../Services/auth-service';
 
 
+
+// metodo auxiliar para retornar o proprio usuario findMe
 export function requestBackend(config: AxiosRequestConfig) {
-  return axios({ ...config, baseURL: Base_URL });
+
+  const headers = config.withCredentials
+    ? {
+      ...config.headers,
+      Authorization: "Bearer " + authService.getAccessToken()
+    }
+    : config.headers;
+
+
+
+
+  return axios({ ...config, baseURL: Base_URL, headers });
 }
+
+
+
