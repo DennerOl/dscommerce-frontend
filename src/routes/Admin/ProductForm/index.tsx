ea2fd11e87b1cb2f918a88945745a3633907e982
DennerOl <dener155@hotmail.com>
2024-01-27 22:06:47 -0300

    Diferenciando create de productId para update

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,10 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./styles.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FormInput from "../../../components/FormInput";
 import * as forms from "../../../utils/forms";
+import * as productService from "../../../Services/product-service";
 
 export default function ProductForm() {
+  const params = useParams();
+
+  const isEditing = params.productId !== "create";
+
   const [formData, setFormData] = useState<any>({
     name: {
       value: "",
@@ -30,6 +35,12 @@ export default function ProductForm() {
       placeholder: "Imagem",
     },
   });
+
+  useEffect(() => {
+    if (isEditing) {
+      productService.findById(Number(params.productId)).then((response) => {});
+    }
+  }, []);
 
   function handleInputChange(event: any) {
     setFormData(forms.update(formData, event.target.name, event.target.value));
