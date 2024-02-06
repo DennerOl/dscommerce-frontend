8207e96d1f3b1b5614ce402a92f7ed95e85dcf37
DennerOl <dener155@hotmail.com>
2024-02-05 21:28:42 -0300

    Styles do select

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -8,6 +8,7 @@ import * as categoryService from "../../../Services/category-service";
 import FormTextArea from "../../../components/FormTextArea";
 import { CategoryDTO } from "../../../models/category";
 import FormSelect from "../../../components/FormSelect";
+import { selectStyles } from "../../../utils/select";
 
 export default function ProductForm() {
   const params = useParams();
@@ -141,7 +142,8 @@ export default function ProductForm() {
               <div>
                 <FormSelect
                   {...formData.categories}
-                  className="dsc-form-control"
+                  className="dsc-form-control dsc-form-select-container"
+                  styles={selectStyles}
                   options={categories}
                   onChange={(obj: any) => {
                     const newFormData = forms.updateAndValidate(
