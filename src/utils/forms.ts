131674f54742d6db0067d1d3145fe9e53825cdf4
DennerOl <dener155@hotmail.com>
2024-02-11 19:23:00 -0300

    Capturando erros do Back-end

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -86,3 +86,16 @@ export function hasAnyInvalid(inputs: any) {
   }
   return false;
 }
+
+// erros do back-end validação
+
+export function setBackendErros(inputs: any, erros: any[]) {
+  const newInputs = { ...inputs };
+  erros.forEach((item) => {
+    newInputs[item.fieldName].message = item.message;
+    newInputs[item.fieldName].dirty = "true";
+    newInputs[item.fieldName].invalid = "true";
+  });
+
+  return newInputs;
+}
